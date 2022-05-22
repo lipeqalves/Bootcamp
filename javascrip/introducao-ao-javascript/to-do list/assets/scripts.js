0e3b66a652edcb90de1ec5d95d3e1afe70caf93e
Filipe <lipeqa@gmail>
2022-05-22 11:27:41 -0300

    adiciona marcadores

--- javascrip/introducao-ao-javascript/to-do list/assets/scripts.js
+++ javascrip/introducao-ao-javascript/to-do list/assets/scripts.js
@@ -1,20 +1,55 @@
 
-let adicionarItem = document.querySelector("#submit")
-let item = document.querySelector("#task-input");
-
-
-adicionarItem.addEventListener("click",function(e){
-   e.preventDefault();
-  
-   let texto = document.createElement('p');
-   let saida = document.querySelector("#tasks");
+    const adicionarItem = document.querySelector("#submit")
+    const item = document.querySelector("#task-input");
+    const marcacao = document.getElementById("tasks");
     
-    texto.innerText = item.value;
-    saida.appendChild(texto);
-    console.log(saida);1
-
-item.value = "";
+adicionarItem.addEventListener("click", (e) =>{
+    e.preventDefault();
+    listas();
+    item.value = "";
 });
+
+const listas = () =>{
+    //let itemLista = document.createElement('li');
+    //let lista = document.createElement("ul");
+    let saida = document.querySelector("#tasks");
+     
+     //itemLista.innerText = item.value;
+     //lista.appendChild(itemLista);
+
+     //saida.appendChild(lista);
+    // console.log(saida);
+     
+    let label = document.createElement('label');
+    label.classList = "lista-Item";
+    let checkbox = document.createElement('input');
+     //label.setAttribute("for", id);
+     checkbox.type = "checkbox";
+     checkbox.name = "lista-Item";
+     checkbox.value = item.value;
+     //checkbox.className = 'checked';
+     label.appendChild(checkbox);
+     label.appendChild(document.createTextNode(item.value));
+     saida.appendChild(label); 
+     //console.log(checkbox.value)
+     console.log(label);
+
+}
+/*
+document.querySelectorAll('li').forEach(createCloseButton);
+
+document.querySelector('ul').addEventListener('click', (e) => {
+  if (e.target.tagName === 'LI')
+    e.target.classList.toggle('checked');
+});
+*/
+marcacao.addEventListener("change", (e) =>{
+    e.preventDefault();
+    let marcar = document.querySelector('.lista-marcar');
+        marcar.classList.toggle('checked')
+    //console.log(checkbox.value)
+})
+
 /*function ChangeTextDecoration () {
     var anchor = document.getElementById ("myAnchor");
     var checkboxes = document.getElementsByName ("checkboxs");
